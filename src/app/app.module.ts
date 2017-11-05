fcc70696402ef2f125041eed4efd4ed356721531
Manpreet <mpsingh17@outlook.co.nz>
2017-11-06 08:53:05 +1300

    Initial commit.

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+// Components.
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { CertificatesComponent } from './components/certificates/certificates.component';
+
+// Route list
+const appRoutes: Routes = [
+  { path: '', component: AboutComponent }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    SkillsComponent,
+    ExperienceComponent,
+    EducationComponent,
+    CertificatesComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
