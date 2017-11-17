f94182ab47aebc2f533c295e658134dbc4cedc7d
Manpreet <mpsingh17@outlook.co.nz>
2017-11-17 17:26:52 +1300

    Service implemented to feed project data.

--- src/app/services/project-data.service.ts
+++ src/app/services/project-data.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Project } from '../models/Project';
 
 @Injectable()
 export class ProjectDataService {
+  projects: Project[] = [
+    {
+      name: 'Vidly',
+      description: 'I made it using ASP.NET MVC5',
+      imagePath: '/images/something.png'
+    },
+    {
+      name: 'Portfolio',
+      description: 'I made it using AngularJS',
+      imagePath: '/images/something.png'
+    }
+  ];
 
   constructor() { }
 
+  // Get all projects.
+  getAllProjects() {
+    return this.projects;
+  }
 }
