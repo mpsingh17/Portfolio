3732961db611a85a89efbb20d3ae3af6005a31ef
Manpreet <mpsingh17@outlook.co.nz>
2017-11-26 22:55:40 +1300

    Setting up projects view.

--- src/app/services/project-data.service.ts
+++ src/app/services/project-data.service.ts
@@ -7,14 +7,14 @@ export class ProjectDataService {
     {
       id: 1,
       name: 'Vidly',
-      description: 'I made it using ASP.NET MVC5',
+      description: 'I\'ve used MVC5 to build this app. It cover required aspects of MVC5 development...',
       imagePath: '/assets/imgs/vidly.png',
       projectURL: 'https://github.com/mpsingh17/Vidly'
     },
     {
       id: 2,
       name: 'Billu Silk Store',
-      description: 'I made it using AngularJS',
+      description: 'This app is built with MVC5 for a cloth shop and can be converted to eStore...',
       imagePath: '/assets/imgs/billuSilkStore.png',
       projectURL: 'https://github.com/mpsingh17/SilkBStore'
     }
